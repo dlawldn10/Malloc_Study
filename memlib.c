68af91a71f879285a23f42cb0bd84777fab745a3
jiwoo <jiwoo@DESKTOP-N03HMBS.localdomain>
2022-10-29 22:54:18 +0900

    Study implicit malloc

--- memlib.c
+++ memlib.c
@@ -17,7 +17,8 @@
 /* private variables */
 static char *mem_start_brk;  /* points to first byte of heap */
 static char *mem_brk;        /* points to last byte of heap */
-static char *mem_max_addr;   /* largest legal heap address */ 
+static char *mem_max_addr;   /* largest legal heap address */
+
 
 /* 
  * mem_init - initialize the memory system model
--- mm.c
+++ mm.c
@@ -44,12 +44,92 @@ team_t team = {
 
 #define SIZE_T_SIZE (ALIGN(sizeof(size_t)))
 
+// 워드 사이즈
+#define WSIZE 4
+// 더블 워드 사이즈
+#define DSIZE 8
+// 초기 가용 블록과 힙 확장을 위한 기본 크기
+#define CHUNKSIZE (1<<12)
+
+// x가 크면 x리턴, t가 크면 y리턴
+#define MAX(x, y) ((x)>(y) ? (x):(y))
+
+// 크기와 할당 비트를 통합해서 헤더와 푸터에 저장할 수 있는 값을 리턴한다.
+// 비트 OR 연산자 |
+// 각 자리수를 비교하여 하나라도 1이 있으면 1이 된다.
+#define PACK(size, alloc) ((size) | (alloc))
+
+// 인자 p가 참조하는 워드를 읽어서 리턴한다.
+#define GET(p) (*(unsigned int *)(p))
+// 인자 p가 가리키는 워드에 val을 저장한다.
+#define PUT(p, val) (*(unsigned int *)(p) = (val))
+
+// 주소 p에 있는 헤더/푸터의 사이즈를 리턴한다.
+#define GET_SIZE(p) (GET(p) & ~0x7)
+// 주소 p에 있는 헤더/푸터의 할당 비트를 리턴한다.
+#define GET_ALLOC(p) (GET(p) & 0x1)
+
+// bp(블록 포인터)가 주어지면
+// 블록 헤더를 가리키는 포인터를 리턴한다.
+#define HDRP(bp) ((char *)(bp) - WSIZE)
+// 푸터를 가리키는 포인터를 리턴한다.
+#define FTRP(bp) ((char *)(bp) + GET_SIZE(HDRP(bp) - DSIZE))
+
+// 다음 블록의 포인터 리턴
+#define NEXT_BLKP(bp) ((char *)(bp) + GET_SIZE(((char *)(bp) - WSIZE)))
+// 이전 블록의 포인터 리턴
+#define PREV_BLKP(bp) ((char *)(bp) - GET_SIZE(((char *)(bp) - DSIZE)))
+
+
+int mm_init(void);
+static void *extend_heap(size_t);
+void *mm_malloc(size_t);
+void mm_free(void *);
+void *mm_realloc(void *, size_t);
+
+
 /* 
  * mm_init - initialize the malloc package.
  */
 int mm_init(void)
 {
+    // 메모리 시스템에서 4워드를 가져와서 초기화한다.
+    if ((heap_listp == mem_sbrk(4*WSIZE)) == (void *)-1) return -1;
+    // alignment padding
+    PUT(heap_listp, 0);
+    // 프롤로그 헤더
+    PUT(heap_listp + (1*WSIZE), PACK(DSIZE, 1));
+    // 프롤로그 푸터
+    PUT(heap_listp + (2*WSIZE), PACK(DSIZE, 1));
+    // 에필로그 헤더
+    PUT(heap_listp + (3*WSIZE), PACK(0,1));
+    heap_listp += (2*WSIZE);
+
+    // 힙을 CHUNKSIZE 바이트로 확장하고 초기 가용 블록을 생성한다.
+    if(extend_heap(CHUNKSIZE/WSIZE) == NULL) return -1;
+
     return 0;
+}
+
+static void *extend_heap(size_t words){
+    char *bp;
+    size_t size;
+
+    // size를 인접 2워드의 배수(8바이트)로 반올림하고
+    // 반올림한 size의 힙 공간을 요청한다.
+    size = (words % 2) ? (words+1) * WSIZE : words * WSIZE;
+    if ((long)(bp = mem_sbrk(size)) == -1) return NULL;
+
+    // 블록 헤더를 free한다.
+    PUT(HDRP(bp), PACK(size, 0));
+    // 블록 푸터를 free한다.
+    PUT(FTRP(bp), PACK(size, 0));
+    // 새 에필로그 헤더
+    PUT(HDRP(NEXT_BLKP(bp)), PACK(0, 1));
+
+    // 두개의 가용 블록을 통합하기 위해 coalese함수 호출하고
+    // 통합된 블록의 블록 포인터를 리턴한다.
+    return coalesce(dp);
 }
 
 /* 
@@ -73,6 +153,40 @@ void *mm_malloc(size_t size)
  */
 void mm_free(void *ptr)
 {
+    size_t size = GET_SIZE(HDRP(bp));
+
+    PUT(HDRP(bp), PACK(size, 0));
+    PUT(FTRP(bp), PACK(size, 0));
+    coalesce(bp);
+}
+
+static void *coalesce(void *bp){
+    size_t prev_alloc = GET_ALLOC(FTRP(PREV_BLKP(bp)));
+    size_t next_alloc = GET_ALLOC(FTRP(PREV_BLKP(bp)));
+    size_t size = GET_SIZE(HDRP(bp));
+
+    if (prev_alloc && next_alloc){
+        return bp;
+    }
+    else if (prev_alloc && !next_alloc){
+        size += GET_SIZE(HDRP(NEXT_BLKP(bp)));
+        PUT(HDRP(bp), PACK(size, 0));
+        PUT(FTRP(bp), PACK(size, 0));
+    }
+    else if (!prev_alloc %% next_alloc){
+        size += GET_SIZE(HDRP(PREV_BLKP(bp)));
+        PUT(FTRP(bp), PACK(size, 0));
+        PUT(HDRP(PREV_BLKP(bp)), PACK(size, 0));
+        bp = PREV_BLKP(bp);
+
+    }
+    else{
+        size += GET_SIZE(HDRP(PREV_BLKP(bp))) + GET_SIZE(FTRP(NEXT_BLKP(bp)));
+        PUT(HDRP(PREV_BLKP(bp)), PACK(size, 0));
+        PUT(FTRP(PREV_BLKP(bp)), PACK(size, 0));
+        bp = PREV_BLKP(bp);
+    }
+    return bp;
 }
 
 /*
